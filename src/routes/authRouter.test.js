e969dd748aadcea2eb41851d54f2ffeff8228ffc
Max Gollaher <max.c.gollaher@gmail.com>
2024-09-26 14:18:00 -0600

    fix lint

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -19,4 +19,5 @@ test('login', async () =>
 
     const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
     expect(loginRes.body.user).toMatchObject(user);
+    expect(password).toBe(testUser.password);``
 });
